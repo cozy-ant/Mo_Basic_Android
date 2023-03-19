93c72d651477cb1a7aec462e7cfa330c8ce7ed35
cozy-ant <koreanant93@gmail.com>
2023-03-19 23:19:05 +0900

    Input event 처리 연습

--- app/src/main/java/com/example/basiccoding/MainActivity.java
+++ app/src/main/java/com/example/basiccoding/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.basiccoding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    Button btn_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_id = findViewById(R.id.et_id);
+        btn_input = findViewById(R.id.btn_input);
+
+        btn_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et_id.setText("Hello World");
+            }
+        });
     }
 }
